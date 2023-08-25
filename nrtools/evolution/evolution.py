corrected strain computation

nrtools/evolution/evolution.py
@@ -171,7 +171,12 @@ class Evolution():
             
         mbh, mns, mtot = id_output.get_msun_masses()
         _, Momg22 = id_output.get_gw_freqs()
-        wm = ev_output.get_mp_Rpsi4(mtot,Momg22)
+        #wm = ev_output.get_mp_Rpsi4(mtot,Momg22)
+        f0 = Momg22 / (2*np.pi) / mtot 
+        dfiles = [os.path.split(x)[1] for x in glob.glob('{}/{}'.format(ev_output.out_inv_dir,'Rpsi4mode??_r*.l0'))]
+        wm = mwaves(path = ev_output.out_inv_dir, code = 'bam', filenames = dfiles, 
+            mass = mtot, f0 = f0,
+            ignore_negative_m=True)
 
         core_out = os.path.join(self.path,'CoReDB')
         try: